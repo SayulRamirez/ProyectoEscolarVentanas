74c2c09ce675dc190bfe874c19e0533eabb99b32
Sayul <sayulramirez@gmail.com>
2023-06-20 23:27:36 -0600

    Agregando el controlador LoginController.java de la ventana Login.fxml con sus componentes y eventos

--- src/main/java/sayul/proyectouno/LoginController.java
+++ src/main/java/sayul/proyectouno/LoginController.java
@@ -6,7 +6,14 @@ package sayul.proyectouno;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -15,6 +22,19 @@ import javafx.fxml.Initializable;
  */
 public class LoginController implements Initializable {
 
+    @FXML
+    private TextField tFieldUsuario;
+    @FXML
+    private PasswordField tFieldPassword;
+    @FXML
+    private CheckBox checkBoxRecordar;
+    @FXML
+    private Button botonInicioSesion;
+    @FXML
+    private Label labelMensaje;
+    @FXML
+    private Label labelContrasenaOlvidada;
+
     /**
      * Initializes the controller class.
      */
@@ -22,5 +42,9 @@ public class LoginController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void actionInicioSesion(ActionEvent event) {
+    }
     
 }
