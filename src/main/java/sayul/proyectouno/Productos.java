596847220bf6a8611306ead95aa5ff40ab00ce47
Sayul <sayulramirez@gmail.com>
2023-06-23 09:07:45 -0600

    Agregado la clase Productos con sus respectivos atributos y getters y setters

---
+++ src/main/java/sayul/proyectouno/Productos.java
@@ -0,0 +1,79 @@
+package sayul.proyectouno;
+
+public class Productos {
+
+private int id;
+private String nombreProducto;
+private int clave;
+private double costo;
+private double precio;
+private int existencia;
+
+public Productos(int id, String nombreProducto, int clave, double costo, double precio, int existencia){
+    this.id = id;
+    this.nombreProducto = nombreProducto;
+    this.clave = clave;
+    this.costo = costo;
+    this.precio = precio;
+    this.existencia = existencia;
+}
+
+public void setId(int id){
+    this.id = id;
+}
+
+public int getId(){
+    return id;
+}
+
+public void setProducto(String producto){
+    this.nombreProducto = producto;
+}
+
+public String setProducto(){
+    return nombreProducto;
+}
+
+public void setClave(int clave){
+    this.clave = clave;
+}
+
+public int getClave(){
+    return clave;
+}
+
+public void  setCosto(double costo){
+    this.costo = costo;
+}
+
+public double getCosto(){
+    return costo;
+}
+
+public void setPrecio(double precio){
+    this.precio = precio;
+}
+
+public double getPrecio(){
+    return precio;
+}
+
+public void setExistencia(int existencia){
+    this.existencia = existencia;
+}
+
+public int getExistencia(){
+    return existencia;
+}
+}
+
+
+
+
+
+
+
+
+
+
+
